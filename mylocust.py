8431cc6f478118932a34dd267426b6c86e3f6386
Huynh Ngoc Bao Vy <vyregister@gmail.com>
2017-12-17 20:34:50 +0700

    Add random-id

--- mylocust.py
+++ mylocust.py
@@ -1,14 +1,25 @@
 from locust import HttpLocust, TaskSet, task
+#know ip address: ip route get 8.8.8.8 | awk '{print $NF; exit}'
 
 class UserBehavior(TaskSet):
     min_wait = 1000
     max_wait = 5000
 
-    @task
-    def post_something(self):
-        self.client.post('/submit')
+    def add_to_list(self):
+        self.client.get('/add')
+
+    # def remove_from_list(self):
+    #     self.client.get('/remove')
+    #
+    # def auth_lumin(self):
+    #     self.client.post('/sign-in',
+    #         {"username": "test123@gmail.com",
+    #          "password": "123456"})
 
 
 class WebsiteUser(HttpLocust):
-    host = "http://127.0.0.1:3000"
+    ## localhost
+    # host = "http://127.0.0.1:3000"
+
+    host = "https://app.luminpdf.com"
     task_set = UserBehavior
--- routes/add.js
+++ routes/add.js
@@ -1,26 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var randomId = require('random-id');
+
 var mongoURI = 'mongodb://admin:123456@ds129146.mlab.com:29146/performance-testing'
 var MongoClient = require('mongodb').MongoClient;
 
-nameArray = ["John", "Jane", "George", "Mary"];
+nameArray = ["Arthur", "Jeanne", "Elizabeth", "Media"];
 genderArray = ["Male", "Female"];
-birthArray = ["1994", "1995", "1996", "1997"];
-cityArray = ["New York", "California", "San Francisco", "Texas"];
+birthArray = ["1850", "1766", "1812", "1001"];
+cityArray = ["London", "Paris", "Slovakia", "Texas"];
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 /* Add one record */
 router.get('/', function(req, res, next) {
 
   // Randomize info
   student = {
+    _id: randomId(),
     name: nameArray[Math.floor(Math.random() * nameArray.length)],
     gender: genderArray[Math.floor(Math.random() * genderArray.length)],
     birthyear: birthArray[Math.floor(Math.random() * birthArray.length)],
     city: cityArray[Math.floor(Math.random() * cityArray.length)]
   };
+  console.log("NAME: " + nameArray[nameArray.length + 1]);
 
   MongoClient.connect(mongoURI, (err, db) => {
     if (err) {
+      console.log(err);
       throw err;
     };
 
@@ -28,7 +37,10 @@ router.get('/', function(req, res, next) {
     db.close();
 
     res.redirect('/');
+
   });
 });
+
+
 
 module.exports = router;
